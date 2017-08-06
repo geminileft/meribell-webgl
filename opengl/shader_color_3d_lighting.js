ca568c0766f3b0f12892e632f43e48adb550e865
Matt Galaviz <matthew.galaviz@gmail.com>
2017-08-05 21:29:56 -0700

    still working

--- opengl/shader_color_3d_lighting.js
+++ opengl/shader_color_3d_lighting.js
@@ -1,39 +1,37 @@
 //TODO: SHADER HAS HARD CODING
 
 const SHADER_COLOR_3D_LIGHTING_VERTEX_SHADER = `
-uniform mat4 uMVPMatrix;
+uniform mat4 u_MVPMatrix;
 uniform mat4 uNormalMatrix;
 
-attribute vec3 aVertices;
-attribute vec4 aColors;
-attribute vec3 aVertexNormal;
+attribute vec3 a_Position;
+attribute vec4 a_Color;
+attribute vec3 a_Normal;
 
-varying vec4 vColor;
-varying vec3 vLighting;
+varying vec4 v_Color;
 
 void main() {
-  gl_Position = uMVPMatrix * vec4(aVertices, 1.0);
-  vColor = aColors;
+  gl_Position = u_MVPMatrix * vec4(a_Position, 1.0);
 
   highp vec3 ambientLight = vec3(0.0, 0.0, 0.0);
   highp vec3 directionalLightColor = vec3(1.0, 1.0, 0.878);
   highp vec3 directionalVector = vec3(0, 0, 1);
 
-  highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+  highp vec4 transformedNormal = uNormalMatrix * vec4(a_Normal, 1.0);
   highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
 
-  vLighting = ambientLight + (directionalLightColor * directional);
+  highp vec3 lighting = ambientLight + (directionalLightColor * directional);
+  v_Color = vec4(a_Color.rgb * lighting, a_Color.a);
 }
 `;
 
 const SHADER_COLOR_3D_LIGHTING_FRAGMENT_SHADER = `
 precision mediump float;
 
-varying vec4 vColor;
-varying vec3 vLighting;
+varying vec4 v_Color;
 
 void main() {
-  gl_FragColor = vec4(vColor.rgb * vLighting, vColor.a);
+  gl_FragColor = v_Color;
 }
 `;
 
@@ -43,9 +41,9 @@ function shader_color_3d_lighting_draw(gl, draw_data) {
   const program_obj = this;
   gl.useProgram(program_obj.program);
 
-  gl.enableVertexAttribArray(program_obj.aVertices);
-  gl.enableVertexAttribArray(program_obj.aColors);
-  gl.enableVertexAttribArray(program_obj.aVertexNormal);
+  gl.enableVertexAttribArray(program_obj.a_Position);
+  gl.enableVertexAttribArray(program_obj.a_Color);
+  gl.enableVertexAttribArray(program_obj.a_Normal);
   
   const interleaved = draw_data.interleaved;
   const buffer = gl.createBuffer();
@@ -55,15 +53,15 @@ function shader_color_3d_lighting_draw(gl, draw_data) {
   const INTERLEAVED_SIZE = GL_VERTEX_SIZE + GL_COLOR_SIZE + GL_NORMAL_SIZE;
 
   gl.vertexAttribPointer(
-    program_obj.aVertices, GL_VERTEX_SIZE, gl.FLOAT, false
+    program_obj.a_Position, GL_VERTEX_SIZE, gl.FLOAT, false
     , INTERLEAVED_SIZE * GL_FLOAT_SIZE_BYTES, 0);
 
   gl.vertexAttribPointer(
-    program_obj.aColors, GL_COLOR_SIZE, gl.FLOAT, false
+    program_obj.a_Color, GL_COLOR_SIZE, gl.FLOAT, false
     , INTERLEAVED_SIZE * GL_FLOAT_SIZE_BYTES, GL_VERTEX_SIZE * GL_FLOAT_SIZE_BYTES);
 
   gl.vertexAttribPointer(
-    program_obj.aVertexNormal, GL_NORMAL_SIZE, gl.FLOAT, false
+    program_obj.a_Normal, GL_NORMAL_SIZE, gl.FLOAT, false
     , INTERLEAVED_SIZE * GL_FLOAT_SIZE_BYTES, (GL_VERTEX_SIZE + GL_COLOR_SIZE) * GL_FLOAT_SIZE_BYTES);
 
   var mvMatrix = mat4.create();
@@ -73,7 +71,7 @@ function shader_color_3d_lighting_draw(gl, draw_data) {
 
   mat4.multiply(draw_data.projectionMatrix, mvMatrix, mvpMatrix);
 
-  gl.uniformMatrix4fv(program_obj.uMVPMatrix, false, mvpMatrix);
+  gl.uniformMatrix4fv(program_obj.u_MVPMatrix, false, mvpMatrix);
 
   var mvInverse = mat4.create();
   var normalMatrix = mat4.create();
@@ -96,8 +94,8 @@ const shader_color_3d_lighting_shader = {
   name: 'shader_color_3d_lighting'
   , vs: SHADER_COLOR_3D_LIGHTING_VERTEX_SHADER
   , fs: SHADER_COLOR_3D_LIGHTING_FRAGMENT_SHADER
-  , attribs: ['aVertices', 'aColors', 'aVertexNormal']
-  , uniforms: ['uMVPMatrix', 'uNormalMatrix']
+  , attribs: ['a_Position', 'a_Color', 'a_Normal']
+  , uniforms: ['u_MVPMatrix', 'uNormalMatrix']
   , draw: shader_color_3d_lighting_draw
 };
 
