7fd716e8b1927d08d6c32accadcd08e3130c7586
Matt Galaviz <matthew.galaviz@gmail.com>
2017-09-03 08:52:01 -0700

    sys_input cleaned

--- sys/sys_input.js
+++ sys/sys_input.js
@@ -1,43 +1,43 @@
-function Sys_Input(width, height, invert_x, invert_y) {
-	this.width = width;
-	this.height = height;
-	this.invert_x = invert_x;
-	this.invert_y = invert_y;
-	this.actions = [];
-	this.handlers = [];
+function Sys_Input(width_in, height_in, invert_x_in, invert_y_in) {
+	this._width = width_in;
+	this._height = height_in;
+	this._invert_x = invert_x_in;
+	this._invert_y = invert_y_in;
+	this._actions = [];
+	this._handlers = [];
 }
 
 Sys_Input.prototype.receiveClickAt = function(x_input, y_input) {
-	var x = this.invert_x ? this.width - x_input : x_input;
-	var y = this.invert_y ? this.height - y_input : y_input;
-	this.actions.push({type:'click', data: {x: x, y: y}});
+	var x = this._invert_x ? this._width - x_input : x_input;
+	var y = this._invert_y ? this._height - y_input : y_input;
+	this._actions.push({type:'click', data: {x: x, y: y}});
 }
 
 Sys_Input.prototype.receiveKeyDown = function(key_input) {
-	this.actions.push({type:'key_down', data: {key: key_input}});
+	this._actions.push({type:'key_down', data: {key: key_input}});
 }
 
 Sys_Input.prototype.receiveKeyUp = function(key_input) {
-	this.actions.push({type:'key_up', data: {key: key_input}});
+	this._actions.push({type:'key_up', data: {key: key_input}});
 }
 
 Sys_Input.prototype.receiveUiEvent = function(event_name) {
-	this.actions.push({type:'ui_event', data: {name:event_name}});
+	this._actions.push({type:'ui_event', data: {name:event_name}});
 }
 
 Sys_Input.prototype.addHandler = function(handler_in) {
-	this.handlers.push(handler_in);
+	this._handlers.push(handler_in);
 }
 
 Sys_Input.prototype.update = function() {
-	var action_results = this.actions;
+	var action_results = this._actions;
 	var result = null;
-	for (var i = 0;i < this.handlers.length;++i) {
-		const handler = this.handlers[i];
+	for (var i = 0;i < this._handlers.length;++i) {
+		const handler = this._handlers[i];
 		action_results  = handler.update(action_results);
 		if (result === undefined || result == null) {
 			break;
 		}
 	}
-	this.actions = [];
+	this._actions = [];
 }
