e5ddefb99c6d1e612941e3a795d4e5f47a7a59f3
Matt Galaviz <matthew.galaviz@gmail.com>
2017-09-03 08:47:06 -0700

    sys_env ivars cleaned

--- sys/sys_env.js
+++ sys/sys_env.js
@@ -1,23 +1,24 @@
 function Sys_Env(canvas_id) {
-	this.canvas = document.getElementById(canvas_id);
-    this.canvas.addEventListener("click", this.getClickPosition.bind(this), false);
-		this.canvas.addEventListener("keydown", this.getKeyDown.bind(this), false);
-		this.canvas.addEventListener("keyup", this.getKeyUp.bind(this), false);
+	const canvas = document.getElementById(canvas_id);
+	canvas.addEventListener("click", this.getClickPosition.bind(this), false);
+	canvas.addEventListener("keydown", this.getKeyDown.bind(this), false);
+	canvas.addEventListener("keyup", this.getKeyUp.bind(this), false);
+	this._canvas = canvas;
 
-    this.imagesLeftToLoad = 0;
-    this.loadedImages = null;
-    this.postLoadCallback = null;
-    this.inputHandler = null;
+	this._imagesLeftToLoad = 0;
+	this._loadedImages = null;
+	this._postLoadCallback = null;
+	this._inputHandler = null;
 }
 
 Sys_Env.prototype.getDimensions = function() {
-	return {width:this.canvas.width, height:this.canvas.height};
+	return {width:this._canvas.width, height:this._canvas.height};
 }
 
 Sys_Env.prototype.createWebGLContext = function() {
 	var context = null;
 	try {
-	    context = this.canvas.getContext("webgl", {stencil:true});
+	    context = this._canvas.getContext("webgl", {stencil:true});
 	} catch (e) {
 	}
 	if (!context) {
@@ -27,11 +28,11 @@ Sys_Env.prototype.createWebGLContext = function() {
 }
 
 Sys_Env.prototype.loadImages = function(images, postLoadCallback) {
-	this.imagesLeftToLoad = images.length;
-	this.postLoadCallback = postLoadCallback;
+	this._imagesLeftToLoad = images.length;
+	this._postLoadCallback = postLoadCallback;
 	var image_map = {};
 
-	if (this.imagesLeftToLoad <= 0) {
+	if (this._imagesLeftToLoad <= 0) {
 		this.postImagesLoad();
 	}
 
@@ -49,30 +50,30 @@ Sys_Env.prototype.loadImages = function(images, postLoadCallback) {
 		image.src = image_name;
 		image_map[image_name] = image;
 	}
-	this.loadedImages = image_map;
+	this._loadedImages = image_map;
 }
 
 Sys_Env.prototype.postImagesLoad = function() {
-	this.postLoadCallback(this.loadedImages, this);
+	this._postLoadCallback(this._loadedImages, this);
 }
 
 Sys_Env.prototype.setInputHandler = function(handler) {
-	this.inputHandler = handler;
+	this._inputHandler = handler;
 }
 
 Sys_Env.prototype.getClickPosition = function(e) {
   var parentPosition = getPosition(e.currentTarget);
   var xPosition = e.clientX - parentPosition.x;
   var yPosition = e.clientY - parentPosition.y;
-  this.inputHandler.receiveClickAt(xPosition, yPosition);
+  this._inputHandler.receiveClickAt(xPosition, yPosition);
 }
 
 Sys_Env.prototype.getKeyDown = function(e) {
-	this.inputHandler.receiveKeyDown(e.key);
+	this._inputHandler.receiveKeyDown(e.key);
 }
 
 Sys_Env.prototype.getKeyUp = function(e) {
-	this.inputHandler.receiveKeyUp(e.key);
+	this._inputHandler.receiveKeyUp(e.key);
 }
 
 function getPosition(el) {
