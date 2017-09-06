c71cab7b80e3819ab858df9d15a9ea96c63e72e5
Matt Galaviz <matthew.galaviz@gmail.com>
2017-09-05 23:54:47 -0700

    removed unused code

--- sys/sys_engine.js
+++ sys/sys_engine.js
@@ -7,53 +7,12 @@ function Sys_Engine(input_in, graphics_in, logic_in) {
     this._scene = null;
 }
 
-Sys_Engine.prototype.addObject = function(game_object) {
-  this._objects.push(game_object);
-}
-
-Sys_Engine.prototype.resetObjects = function() {
-  for (var i = 0;i < this._objects.length;++i) {
-    const game_object = this._objects[i];
-    game_object.clearMessages();
-  }
-}
-
 Sys_Engine.prototype.start = function(scene) {
 
   this._scene = scene;
   const input = this._input;
   var gfx = this._graphics;
   const logic = this._logic;
-
-
-  /*
-  for (var i = 0;i < scene_objects.length;++i) {
-    this.addObject(scene_objects[i]);
-  }
-  */
-
-  /*
-  for (var j = 0;j < this._objects.length;++j) {
-    const object = this._objects[j];
-    for(var i = 0;i < object.handlers.length;++i) {
-      const handler = object.handlers[i];
-      switch (handler.sys) {
-        case 'graphics':
-          gfx.addHandler(handler);
-        break;
-        case 'input':
-          input.addHandler(handler);
-        break;
-        case 'logic':
-          logic.addHandler(handler);
-        break;
-        default:
-          alert('Sys not defined for handler!');
-        break;
-      }
-    }
-  }
-  */
 
   const map = {
     graphics : gfx
@@ -76,7 +35,6 @@ function run_impl() {
   gfx.update();
 
   gfx.drawScene();
-  //this.resetObjects();
   this._scene.reset_objects();
 
   window.setTimeout(this._run, 1000 / 60);
