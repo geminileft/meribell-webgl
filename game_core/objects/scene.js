e583c3dc1a7b0b063cb5ce9a25a0d3100356b2a0
Matt Galaviz <matthew.galaviz@gmail.com>
2017-09-05 23:35:31 -0700

    currently working, ready for change

---
+++ game_core/objects/scene.js
@@ -0,0 +1,28 @@
+function Scene(objects_in) {
+    this._objects = objects_in;
+}
+  
+Scene.prototype.reset_objects = function() {
+    for (var i = 0;i < this._objects.length;++i) {
+      const game_object = this._objects[i];
+      game_object.clearMessages();
+    }
+}
+/*
+system map is a dictionary
+with key = name of system
+    input, logic, graphics
+*/
+
+Scene.prototype.start = function(system_map) {
+    for (var j = 0;j < this._objects.length;++j) {
+        const object = this._objects[j];
+        for(var i = 0;i < object.handlers.length;++i) {
+            const handler = object.handlers[i];
+            const sys = system_map[handler.sys];
+            //TODO: TEST FOR INVALID SYS
+            sys.addHandler(handler);
+        }
+    }
+    this._run();
+}
