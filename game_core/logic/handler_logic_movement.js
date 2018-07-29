c7a0c45de6b3415971da34542a1cf0c267862397
Matt Galaviz <matthew.galaviz@gmail.com>
2018-07-28 20:51:17 -0700

    added additional objects and changed movement to eliminate hard coding

--- game_core/logic/handler_logic_movement.js
+++ game_core/logic/handler_logic_movement.js
@@ -1,16 +1,33 @@
-function Handler_Logic_Movement() {
+function Handler_Logic_Basic_Movement(x_delta_inc, y_delta_inc, z_delta_inc, x_range, y_range, z_range) {
     this.sys = 'logic';
-    this.delta = 0;
-    this.pos_delta = 0.25;
+    this.x_delta = 0;
+    this.y_delta = 0;
+    this.z_delta = 0;
+    this.x_delta_inc = x_delta_inc;
+    this.y_delta_inc = y_delta_inc;
+    this.z_delta_inc = z_delta_inc;
+    this.x_range = x_range;
+    this.y_range = y_range;
+    this.z_range = z_range;
 }
 
-Handler_Logic_Movement.prototype.update = function() {
+Handler_Logic_Basic_Movement.prototype.update = function() {
     const game_object = this.game_object;
 
-    this.delta += this.pos_delta;
-    game_object.x += this.pos_delta;
+    this.x_delta += this.x_delta_inc;
+    this.y_delta += this.y_delta_inc;
+    this.z_delta += this.z_delta_inc;
+    game_object.x += this.x_delta_inc;
+    game_object.y += this.y_delta_inc;
+    game_object.z += this.z_delta_inc;
 
-    if (this.delta > 10 || this.delta < -10) {
-        this.pos_delta *= -1;
+    if (this.x_delta > this.x_range || this.x_delta < -this.x_range) {
+        this.x_delta_inc *= -1;
+    }
+    if (this.y_delta > this.y_range || this.y_delta < -this.y_range) {
+        this.y_delta_inc *= -1;
+    }
+    if (this.z_delta > this.z_range || this.z_delta < -this.z_range) {
+        this.z_delta_inc *= -1;
     }
 }
--- game_core/scenes/scene_jolene.js
+++ game_core/scenes/scene_jolene.js
@@ -10,26 +10,7 @@ function Scene_Jolene3(width, height) {
   var handler_logic;
   var handler_input;
 
-  const color_range = [
-      [0.0, 0.0, 1.0, 1.0]
-      , [0.0, 1.0, 0.0, 1.0]
-      , [1.0, 0.0, 0.0, 1.0]
-      , [1.0, 0.75, 0.75, 1.0]
-      , [1.0, 0.0, 1.0, 1.0]
-      , [0.0, 1.0, 1.0, 1.0]
-  ];
-
   game_object = new Game_Object(10, 12, 20);
-
-  const camera_location = {
-        x_pos: 10
-        , x_rot: 0
-        , y_pos: 12
-        , y_rot: 0
-        , z_pos: 20
-        , z_rot: 0
-    };
-
 
   matrix_handler = new Handler_Graphics_Matrix(width, height, 40, .1, 100);
   game_object.addHandler(matrix_handler);
@@ -43,7 +24,23 @@ function Scene_Jolene3(width, height) {
   handler_graphics = new Handler_Graphics_3dbox(0, 0
     , [[1.0, 1.0, 1.0, 1.0]], matrix_handler);
   game_object.addHandler(handler_graphics);
-  handler_logic = new Handler_Logic_Movement();
+  handler_logic = new Handler_Logic_Basic_Movement(0.25, 0, 0, 10, 0, 0);
+  game_object.addHandler(handler_logic);
+  game_objects.push(game_object);
+
+  game_object = new Game_Object(-10, -1, -8);
+  handler_graphics = new Handler_Graphics_3dbox(0, 0
+    , [[1.0, 1.0, 1.0, 1.0]], matrix_handler);
+  game_object.addHandler(handler_graphics);
+  handler_logic = new Handler_Logic_Basic_Movement(0, 0.25, 0, 0, 10, 0);
+  game_object.addHandler(handler_logic);
+  game_objects.push(game_object);
+
+  game_object = new Game_Object(-10, -1, 0);
+  handler_graphics = new Handler_Graphics_3dbox(0, 0
+    , [[1.0, 1.0, 1.0, 1.0]], matrix_handler);
+  game_object.addHandler(handler_graphics);
+  handler_logic = new Handler_Logic_Basic_Movement(0, 0, 0.25, 0, 0, 10);
   game_object.addHandler(handler_logic);
   game_objects.push(game_object);
 
