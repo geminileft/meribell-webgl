7f1fb8684b1672bd384e37b8068a16963c2f85b2
Matt Galaviz <matthew.galaviz@gmail.com>
2018-09-09 13:51:14 -0700

    two showing up

--- game_core/graphics/handler_graphics_singlestreet.js
+++ game_core/graphics/handler_graphics_singlestreet.js
@@ -40,7 +40,10 @@ Handler_Graphics_Singlestreet.prototype.getData = function() {
 	const lanes = 1;
 
 	const spacing = 4.0;
+	//TODO: REMOVE BELOW AFTER REPLACING WITH STARTING RIGHT POSITION
 	const half_spacing = (lanes * spacing) / 2.0;
+	const half_sp2 = spacing / 2.0;
+	var right_position = (lanes * spacing) / 2.0;
 
 	const width = this.width;
 	const half_width = width / 2.0;
@@ -58,31 +61,30 @@ Handler_Graphics_Singlestreet.prototype.getData = function() {
 		, 0.0000, -1.0000, 0.0000
 	];
 
-	var my_normals = STANDARD_NORMAL;
+	var my_normals = [];
+	var my_vertices = [];
 
-	for(var i = 0;i < lanes;++i) {
+	for(var i = 0;i < lanes + 1;++i) {
 		my_normals.push.apply(my_normals, STANDARD_NORMAL);
 	}
 
+	for(var i = 0;i < lanes + 1;++i) {
+		const STANDARD_VERTEX = [
+			-right_position + half_width, 0.0, -half_height
+			, -right_position + half_width, 0.0, half_height
+			, -right_position -half_width, 0.0, half_height
+			, -right_position + half_width, 0.0, -half_height
+			, -right_position -half_width, 0.0, half_height
+			, -right_position -half_width, 0.0, -half_height		
+		];
+		my_vertices.push.apply(my_vertices, STANDARD_VERTEX);
+		right_position -= spacing;
+	}
+
 	return {
-vertices : [
-	-half_spacing + half_width, 0.0, -half_height
-	, -half_spacing + half_width, 0.0, half_height
-	, -half_spacing -half_width, 0.0, half_height
-	, -half_spacing + half_width, 0.0, -half_height
-	, -half_spacing -half_width, 0.0, half_height
-	, -half_spacing -half_width, 0.0, -half_height
-
-	, half_spacing + half_width, 0.0, -half_height
-	, half_spacing + half_width, 0.0, half_height
-	, half_spacing -half_width, 0.0, half_height
-	, half_spacing + half_width, 0.0, -half_height
-	, half_spacing -half_width, 0.0, half_height
-	, half_spacing -half_width, 0.0, -half_height
-
-]
+vertices : my_vertices
 
 , normals : my_normals
-, count : 2 * SINGLE_VERTEX_COUNT
+, count : (lanes + 1) * SINGLE_VERTEX_COUNT
 };
 }
