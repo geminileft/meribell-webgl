63bc791338f3a61f82887fe4d0fb087635e972b1
Matt Galaviz <matthew.galaviz@gmail.com>
2018-09-09 14:29:39 -0700

    got lines extending out

--- game_core/graphics/handler_graphics_singlestreet.js
+++ game_core/graphics/handler_graphics_singlestreet.js
@@ -37,19 +37,6 @@ Handler_Graphics_Singlestreet.prototype.update = function(gfx) {
 }
 
 Handler_Graphics_Singlestreet.prototype.getData = function() {
-	const lanes = 3;
-
-	const spacing = 4.0;
-	//TODO: REMOVE BELOW AFTER REPLACING WITH STARTING RIGHT POSITION
-	const half_spacing = (lanes * spacing) / 2.0;
-	const half_sp2 = spacing / 2.0;
-	var right_position = (lanes * spacing) / 2.0;
-
-	const width = this.width;
-	const half_width = width / 2.0;
-	const height = this.height;
-	const half_height = height / 2.0;
-
 	const SINGLE_VERTEX_COUNT = 6;
 
 	const STANDARD_NORMAL = [
@@ -61,30 +48,49 @@ Handler_Graphics_Singlestreet.prototype.getData = function() {
 		, 0.0000, -1.0000, 0.0000
 	];
 
-	var my_normals = [];
-	var my_vertices = [];
+	// TODO: REFACTOR TO BE PARAMETERS
+	const lanes = 3;
+	const rows = 20;
+	const spacing = 4.0;
+	const top_spacing = 1.85;
 
+
+	var right_position = (lanes * spacing) / 2.0;
+	var top_position = 0;
+	const width = this.width;
+	const half_width = width / 2.0;
+	const height = this.height;
+	const half_height = height / 2.0;
+
+	var my_normals = [];
 	for(var i = 0;i < lanes + 1;++i) {
-		my_normals.push.apply(my_normals, STANDARD_NORMAL);
+		for (var j = 0;j < rows;++j) {
+			my_normals.push.apply(my_normals, STANDARD_NORMAL);
+		}
 	}
 
-	for(var i = 0;i < lanes + 1;++i) {
-		const STANDARD_VERTEX = [
-			-right_position + half_width, 0.0, -half_height
-			, -right_position + half_width, 0.0, half_height
-			, -right_position -half_width, 0.0, half_height
-			, -right_position + half_width, 0.0, -half_height
-			, -right_position -half_width, 0.0, half_height
-			, -right_position -half_width, 0.0, -half_height		
-		];
-		my_vertices.push.apply(my_vertices, STANDARD_VERTEX);
-		right_position -= spacing;
+	var my_vertices = [];
+	for (var j = 0;j < rows;++j) {
+		right_position = (lanes * spacing) / 2.0;
+		for(var i = 0;i < lanes + 1;++i) {
+			const STANDARD_VERTEX = [
+				-right_position + half_width, 0.0, -top_position -half_height
+				, -right_position + half_width, 0.0, -top_position + half_height
+				, -right_position -half_width, 0.0, -top_position + half_height
+				, -right_position + half_width, 0.0, -top_position - half_height
+				, -right_position -half_width, 0.0, -top_position + half_height
+				, -right_position -half_width, 0.0, -top_position - half_height		
+			];
+			my_vertices.push.apply(my_vertices, STANDARD_VERTEX);
+			right_position -= spacing;
+		}
+		top_position += (height * top_spacing);
 	}
 
 	return {
 vertices : my_vertices
 
 , normals : my_normals
-, count : (lanes + 1) * SINGLE_VERTEX_COUNT
+, count : (lanes + 1) * SINGLE_VERTEX_COUNT * rows
 };
 }
